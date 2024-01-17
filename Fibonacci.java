fbfbd7dc9d1b1f78961badc4686616c0b21fd750
anthonypham2 <143339682+anthonypham2@users.noreply.github.com>
2024-01-16 21:44:42 -0500

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util;
+
+public class Fibonacci{
+    public static int fibonacciSequence(int input){
+            if(input <= 1){
+                return input;
+            }
+            else{
+                return fibonacciSequence(input - 1) + fibonacciSequence(input - 2);
+            }
+    }    
+        
+    public static void main(String args[]){
+        int input = 10;
+            
+        System.out.println("The " + input + " term of the fibonacciSequenceonacci sequence is " + fibonacciSequence(input));
+    }
+}
+
+
